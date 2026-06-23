31b521d8b91b13554b61d99734df3b495cef70c9
Provaxcer <basaksantanu659@gmail.com>
2026-06-23 13:00:10 +0530

    day 2 react

--- day1/my-react-app/src/App.jsx
+++ day1/my-react-app/src/App.jsx
@@ -1,9 +1,14 @@
 function Portfolio() {
+const name = "Santanu";
+const value = 150;
+const coins = 1;
+
   return (
     <div>
-      <p>Santanu</p>
-      <p>Value - 69</p>
-      <p>Coins - 1</p>
+      <p>{name}</p>
+      <p>{value}</p>
+      <p>{coins}</p>
+      <p>{value > 100? "Up" : "Down"}</p>
     </div>
   )
 }
