e6125e351092e06584c20bda598e55b43803e4dc
Provaxcer <basaksantanu659@gmail.com>
2026-06-26 16:54:42 +0530

    usestate

--- day3/my-react-app/src/App.jsx
+++ day3/my-react-app/src/App.jsx
@@ -1,19 +1,16 @@
-function TraderProfile() {
-  return(
+import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
     <div>
-      <TraderCard name="Santanu" capital={500} profitpercentage={5} />
-      <TraderCard name="Zoro" capital={100} profitpercentage={2} />
-      <TraderCard name="Luffy" capital={400} profitpercentage={1} />
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>
+        Add
+      </button>
     </div>
-  )
+  );
 }
 
-function TraderCard(props) {
-  return(
-    <div>
-      <h1>{props.name}</h1>
-      <h3>{props.capital}</h3>
-      <h3>{props.profitpercentage}</h3>
-    </div>  
-  )
-}
+export default Counter;
