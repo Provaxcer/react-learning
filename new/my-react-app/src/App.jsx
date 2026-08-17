beaa6b1f85f817842e12bf21506605ab8cfcb6fd
Provaxcer <basaksantanu659@gmail.com>
2026-08-17 18:19:22 +0530

    react

--- new/my-react-app/src/App.jsx
+++ new/my-react-app/src/App.jsx
@@ -1,19 +1,35 @@
-function Cookie(props) {
-  return <div>{props.flavour} cookies</div>;
-}
+import { useState } from "react";
 
-function Shop() {
+// function Cookie(props) {
+//   return <div>{props.flavour} cookies</div>;
+// }
+
+// function Shop() {
+//   return (
+//     <div>
+//       <Cookie flavour="Chocolate Chip" />
+//       <Cookie flavour="Oatmeal" />
+//       <Cookie flavour="Sugar" />
+//     </div>
+//   );
+// }
+
+// function App() {
+//   return <Shop />;
+// }
+
+function Counter() {
+  const [count, increaseCount] = useState(0);
   return (
     <div>
-      <Cookie flavour="Chocolate Chip" />
-      <Cookie flavour="Oatmeal" />
-      <Cookie flavour="Sugar" />
+      <p>Cookies Baked: {count}</p>
+      <button onClick={() => increaseCount(count + 1)}>Bake a cookie</button>
     </div>
-  );
+  )
 }
 
-function App() {
-  return <Shop />;
+function App () {
+  return (<Counter />)
 }
 
 export default App;
