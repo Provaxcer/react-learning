f92e16774eaa94103b6c76bc9410f75e613dd96d
Provaxcer <basaksantanu659@gmail.com>
2026-09-22 19:02:44 +0530

    react

--- R2/usestate/src/App.jsx
+++ R2/usestate/src/App.jsx
@@ -2,22 +2,45 @@ import { useState } from "react";
 
 function App() {
   const [tasks, setTasks] = useState([
-    "Learn React",
-    "Build a website",
-    "Practice JavaScript",
-    "Build a React project",
+    { name: "Learn React", completed: false },
+    { name: "Build a website", completed: false },
+    { name: "Practice JavaScript", completed: false },
   ]);
 
   const [input, setInput] = useState("");
 
   function addTask() {
-    setTasks([...tasks, input]);
+    setTasks([
+      ...tasks,
+      {
+        name: input,
+        completed: false,
+      },
+    ]);
   }
 
-  function deleteTask(){
-    const result = tasks.filter((num)=>{
-      return num == key
-    })
+  function deleteTask(taskToDelete) {
+    const result = tasks.filter((task) => {
+      return task.name !== taskToDelete.name;
+    });
+
+    setTasks(result);
+  }
+
+  function completeTask(taskToComplete) {
+    const result = tasks.map((task) => {
+      if (task.name === taskToComplete.name) {
+        return {
+          ...task,
+          completed: true,
+        };
+      }
+
+      return task;
+    });
+
+    setTasks(result);
+  }
 
   return (
     <div>
@@ -29,17 +52,30 @@ function App() {
           setInput(e.target.value);
         }}
       />
+
       <button onClick={addTask}>Add Task</button>
+
       <h1>Tasks:</h1>
+
       <div>
         {tasks.map((allTask) => {
-          return (
-            <div>
-              <p key={allTask}>{allTask}</p>
-              <button onClick={deleteTask}>Delete</button>
-            </div>
-          );
-        })}
+  return (
+    <div key={allTask.name}>
+      <p>
+        {allTask.completed ? "✓ " : ""}
+        {allTask.name}
+      </p>
+
+      <button onClick={() => deleteTask(allTask)}>
+        Delete
+      </button>
+
+      <button onClick={() => completeTask(allTask)}>
+        Complete
+      </button>
+    </div>
+  );
+})}
       </div>
     </div>
   );
